ac6b9b1039874730a52656e089e8a023308f1c79
haro001 <mica01120@gmail.com>
2026-03-17 12:01:49 +0900

    Set up Gradle build environment and project package structure

---
+++ src/main/java/com/editor/client/ClientMain.java
@@ -0,0 +1,8 @@
+package com.editor.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("=== Shared Text Editor Client ===");
+        // TODO: 서버 연결, 로그인 UI 실행
+    }
+}
---
+++ src/main/java/com/editor/server/ServerMain.java
@@ -0,0 +1,8 @@
+package com.editor.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("=== Shared Text Editor Server ===");
+        // TODO: 서버 소켓 리스닝, 클라이언트 접속 관리
+    }
+}
